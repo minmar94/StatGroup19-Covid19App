2a3b6703903e4ffbf8209126d157c3712a90c5d8
MarcoMingio <marco.mingione94@gmail.com>
2020-05-08 18:09:21 +0200

    Delete DeployApp.R

--- Script/DeployApp.R
+++
@@ -1,7 +0,0 @@
-rsconnect::deployApp("C:/Users/marco/OneDrive/Desktop/Marco/Universita/StatGroup19Eng", forceUpdate = T, 
-                     appName = "StatGroup19Eng", account = "marcomingione")
-
-
-rsconnect::deployApp("C:/Users/marco/OneDrive/Desktop/Marco/Universita/StatGroup19Eng", forceUpdate = T, 
-                     appName = "StatGroup19-Eng", account = "statgroup19")
-
